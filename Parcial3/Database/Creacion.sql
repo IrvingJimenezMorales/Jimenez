553ef75d23ac0303b6e767b4c4b9749fdb0934d8
IrvingJimenezMorales <47284981+IrvingJimenezMorales@users.noreply.github.com>
2021-11-19 18:26:14 -0600

    Actualizacion

--- Parcial3/Database/Creacion.sql
+++ Parcial3/Database/Creacion.sql
@@ -1,6 +1,8 @@
-create database L16100220;
-use L16100220;
+--creacion de la base de datos
+create database Web16100220;
+use Web16100220;
 
+-- creacion de la tabla usuarios
 create table Usuarios
 (
     ID_Usuario smallint  not null auto_increment ,
@@ -9,8 +11,9 @@ create table Usuarios
     pass varchar(30),
     primary key(ID_Usuario)
 
-)
+);
 
+--creacion de la tabla operadores
 create table Operadores
 (
    IdOperador smallint   not null auto_increment ,
@@ -26,8 +29,10 @@ create table Operadores
    HoraEntrada Time,
    CodigoBarras int(6),
    primary key(IdOperador)
-)
+   
+);
 
+--insercion de valores a la tabla usuarios
 insert into Usuarios(Usuario,Nombre,Pass)
 values ('Administrador','Francisco Hernandez','Admin2021');
 
@@ -37,6 +42,7 @@ values ('Sistemas','Irving Jimenez','Sis2021');
 insert into Usuarios(Usuario,Nombre,Pass)
 values ('Gerencia','Sara Garcia','GerenAux21');
 
+-- insercion de valores a la tabla Operadores
 insert into Operadores(NombreOperador,ApellidoOperador,Telefono,NumCaja,Departamento,Pedimento,Patente,Folio,FechaEntrada,HoraEntrada,CodigoBarras)
 values("Francisco","Velazquez","8671236549","MVR56","SAGARPA","78998745","1234","1347654",NOW(),NOW(),"000001");
 
